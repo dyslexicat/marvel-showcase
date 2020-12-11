569daf274be1b53577294e8e0e96133ae7ddceac
dyslexicat <dyslexicat@gmail.com>
2020-12-11 18:03:41 +0300

    Changed fetching comics from REGEX to API calls

--- src/components/Comics.js
+++ src/components/Comics.js
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from "react";
 
-const Comics = () => {
+// previously I used a regex to match the comics from the info we already had but this is less error prone
+const Comics = ({ characterID }) => {
   const API_KEY = process.env.REACT_APP_MARVEL_API_KEY;
   const [comics, setComics] = useState(null);
+  const [isLoading, setLoading] = useState(true);
 
   const fetchComics = async () => {
     const startDate = "2015-01-01";
     const today = new Date().toISOString().split("T", 1)[0];
     const dateToSend = `${startDate}%2C${today}`;
-    const url = `https://gateway.marvel.com:443/v1/public/characters/1009148/comics?format=comic&formatType=comic&dateRange=${dateToSend}&orderBy=onsaleDate&apikey=${API_KEY}`;
+    const url = `https://gateway.marvel.com:443/v1/public/characters/${characterID}/comics?format=comic&formatType=comic&dateRange=${dateToSend}&orderBy=-focDate&apikey=${API_KEY}`;
 
     const response = await fetch(url);
 
@@ -20,8 +22,8 @@ const Comics = () => {
     const json = await response.json();
 
     const comics = json.data.results.map((comic) => comic.title);
-    console.log(comics);
-    setComics(comics);
+    setComics(comics.slice(0, 10));
+    setLoading(false);
   };
 
   useEffect(() => {
@@ -30,10 +32,12 @@ const Comics = () => {
 
   return (
     <div>
-      {comics ? (
+      {isLoading ? (
         <p>Loading Comics</p>
+      ) : comics.length === 0 ? (
+        <p>No comics to show</p>
       ) : (
-        comics.map((comic) => <p key="idx">{comic}</p>)
+        comics.map((comic, idx) => <div key={idx}>{comic}</div>)
       )}
     </div>
   );
--- src/pages/CharacterDetails.js
+++ src/pages/CharacterDetails.js
@@ -2,6 +2,7 @@ import React, { useContext, useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Context } from "../context/store";
 import HomeButton from "../components/HomeButton";
+import Comics from "../components/Comics";
 
 const CharacterDetails = () => {
   const [character, setCharacter] = useState(null);
@@ -9,7 +10,7 @@ const CharacterDetails = () => {
   const [{ characters }] = useContext(Context);
 
   useEffect(() => {
-    const filtered =  characters.filter(
+    const filtered = characters.filter(
       (character) => character.id === parseInt(characterID)
     );
     const character = filtered[0];
@@ -17,53 +18,13 @@ const CharacterDetails = () => {
     setCharacter(character);
   }, []);
 
-  // I don't want to send a request to the MARVEL API everytime a user clicks details
-  // that is why I wrote this function like this. Otherwise, we can use the
-  // /v1/public/characters/{characterId}/comics endpoint
-  const getComics = () => {
-    const yearRegex = /\([0-9]{4}\)/;
-    const years = [];
-
-    // use regex to filter hero comics by year
-    // I used a generalized regex so that there aren't any problems
-    character.comics.items.forEach((comic, idx) => {
-      const match = comic.name.match(yearRegex);
-      if (match) {
-        const yearInt = parseInt(match[0].slice(1, match[0].length - 1));
-        if (yearInt >= 2005) {
-          years.push([idx, yearInt]);
-        }
-      }
-    });
-
-    // sort the years by descending
-    years.sort(function (a, b) {
-      const keyA = a[1];
-      const keyB = b[1];
-      if (keyA < keyB) {
-        return 1;
-      }
-      if (keyB < keyA) {
-        return -1;
-      }
-      return 0;
-    });
-
-    // get the first 10 elements
-    const ids = years.slice(0, 10).map((item) => item[0]);
-    const comics = [];
-    for (const id of ids) {
-      comics.push(character.comics.items[id]);
-    }
-
-    return comics;
-  };
-
   return (
     <div className="main-div">
       <HomeButton character={character} />
       {!character ? (
-        <p className="error-message">There is no info for this Marvel Character</p>
+        <p className="error-message">
+          There is no info for this Marvel Character
+        </p>
       ) : (
         <div className="hero-details">
           <h2>{character.name}</h2>
@@ -73,9 +34,7 @@ const CharacterDetails = () => {
             className="hero-image"
           />
           <p>{character.description}</p>
-          {getComics().map((comic, id) => (
-            <div key={id}>{comic.name}</div>
-          ))}
+          <Comics characterID={character.id} />
         </div>
       )}
     </div>
